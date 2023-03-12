e1bc29c7312d5d282d88baeb1067ed2ce211300b
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-12 16:29:53 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,16 +5,18 @@ const body = document.querySelector('body');
 startBtn.addEventListener('click', startBtnActive);
 stopBtn.addEventListener('click', stopBtnActive);
 
+let timerId = 0;
+
 function startBtnActive(e) {
-  timerId = setInterval(() => {
+  return (timerId = setInterval(() => {
     body.style.backgroundColor = `#${Math.floor(
       Math.random() * 16777215
     ).toString(16)}`;
-  }, 1000);
-  startBtn.setAttribute('disabled', 'disabled');
+    startBtn.setAttribute('disabled', 'disabled');
+  }, 1000));
 }
 
-function stopBtnActive(e) {
+function stopBtnActive() {
   clearTimeout(timerId);
   startBtn.removeAttribute('disabled');
 }
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,10 +3,25 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const timerEl = document.querySelector('.timer');
 const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
 
 timerEl.style.gap = '10px';
 timerEl.style.display = 'flex';
 startBtn.setAttribute('disabled', 'disabled');
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  daysEl.textContent = `${pad(days)}`;
+  hoursEl.textContent = `${pad(hours)}`;
+  minutesEl.textContent = `${pad(minutes)}`;
+  secondsEl.textContent = `${pad(seconds)}`;
+}
 
 const options = {
   enableTime: true,
@@ -24,7 +39,9 @@ const options = {
     function startTimer(e) {
       const timerId = setInterval(() => {
         const startTime = selectedDates[0] - Date.now();
-    
+        if (selectedDates[0].getTime() - Date.now() <= 1000) {
+          clearTimeout(timerId);
+        }
         function convertMs(ms) {
           const second = 1000;
           const minute = second * 60;
@@ -42,8 +59,7 @@ const options = {
           return { days, hours, minutes, seconds };
         }
 
-        console.log(convertMs(startTime));
-        
+        addLeadingZero(convertMs(startTime));
       }, 1000);
     }
   },
