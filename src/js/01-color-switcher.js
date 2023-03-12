f83db24d38707641fd679bc4cdc845a2b924be2a
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-12 11:49:20 +0200

    Update 01-color-switcher.js
    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,7 +10,6 @@ function startBtnActive(e) {
     body.style.backgroundColor = `#${Math.floor(
       Math.random() * 16777215
     ).toString(16)}`;
-    console.log(rundomColor);
   }, 1000);
   startBtn.setAttribute('disabled', 'disabled');
 }
