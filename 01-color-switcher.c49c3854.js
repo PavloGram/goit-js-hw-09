152b263dbc54652a8cafbb7df68744d139a6c4cc
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-12 14:30:22 +0000

    Deploying to gh-pages from @ PavloGram/goit-js-hw-09@e1bc29c7312d5d282d88baeb1067ed2ce211300b 🚀

--- 01-color-switcher.c49c3854.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(e){timerId=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(e){clearTimeout(timerId),t.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.c49c3854.js.map
