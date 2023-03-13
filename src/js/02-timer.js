8683bc60090b30c49c7b0c0c88a3e1fa70679634
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-13 17:43:18 +0200

    2.5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -39,7 +39,7 @@ const options = {
     function startTimer(e) {
       const timerId = setInterval(() => {
         const startTime = selectedDates[0] - Date.now();
-        if (selectedDates[0].getTime() - Date.now() <= 1000) {
+        if (selectedDates[0].getTime() - Date.now() === 1000) {
           clearTimeout(timerId);
         }
         function convertMs(ms) {
@@ -47,15 +47,10 @@ const options = {
           const minute = second * 60;
           const hour = minute * 60;
           const day = hour * 24;
-
           const days = Math.floor(ms / day);
-
           const hours = Math.floor((ms % day) / hour);
-
           const minutes = Math.floor(((ms % day) % hour) / minute);
-
           const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
           return { days, hours, minutes, seconds };
         }
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', formValue);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
+
+function formValue(eve) {
+  eve.preventDefault();
+  const firstDelay = eve.target.delay.value;
+  const delayStep = eve.target.step.value;
+  const amount = eve.target.amount.value;
+
+  let counter = 0;
+  let stepCounter = Number(delayStep);
+
+  const int = setInterval(() => {
+    counter += 1;
+    stepCounter += Number(firstDelay);
+
+    createPromise(delayStep, amount)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${counter} in ${stepCounter}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${counter} in ${stepCounter}ms`);
+      });
+    if (counter >= amount) {
+      clearInterval(int);
+    }
+    console.log(stepCounter);
+  }, firstDelay);
 }
